aa3dce266a21ff8cffc5c156077517d9345eea44
Niraj Kamdar <nirajkamdar27@pm.me>
2023-06-29 02:33:26 +0700

    fix: linting issues

--- packages/polywrap-sys-config-bundle/polywrap_sys_config_bundle/types/embedded_file_reader.py
+++ packages/polywrap-sys-config-bundle/polywrap_sys_config_bundle/types/embedded_file_reader.py
@@ -1,5 +1,6 @@
 """This module contains the embedded file reader."""
 from pathlib import Path
+
 from polywrap_core import FileReader
 
 
@@ -7,9 +8,11 @@ class EmbeddedFileReader(FileReader):
     """A file reader that reads from the embedded files."""
 
     def __init__(self, embedded_wrap_path: Path):
+        """Initialize the embedded file reader."""
         self._embedded_wrap_path = embedded_wrap_path
 
     def read_file(self, file_path: str) -> bytes:
+        """Read the file from the embedded files."""
         with open(self._embedded_wrap_path / file_path, "rb") as f:
             return f.read()
 
